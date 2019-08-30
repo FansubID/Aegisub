a958faea51139fab223f5009b9ec62ab5d86d8cd
odrling <florianbadie@odrling.xyz>
2019-08-31 00:16:59 +0200

    [appveyor] upload build via ftp part 3

--- .ci/upload_win_installer.sh
+++ .ci/upload_win_installer.sh
@@ -1,11 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 dest="ftp://shelter.mahoro-net.org/aegisub-japan7"
-
 tag=$(git describe --exact-match)
 [ "$tag" ] || exit
+
 curl -T 'packages\win_installer\output\Aegisub-Japan7-x64.exe' --user $FTP_USER:$FTP_PASS "$dest/Aegisub-Japan7-x64-${tag#v}.exe"
-
-printf "${tag#v}\n$(git tag -l --format='%contents' $tag)" > latest
-curl -T latest --user "$FTP_USER:$FTP_PASS" "$dest/"
-
+curl -T <(printf "${tag#v}\n$(git tag -l --format='%contents' $tag)") --user "$FTP_USER:$FTP_PASS" "$dest/"
