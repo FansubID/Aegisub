0953d23168b956359401ccaebc4875b9b10d86c9
odrling <florianbadie@odrling.xyz>
2019-08-30 23:45:07 +0200

    [appveyor] auto ftp upload part 2

--- .ci/upload_win_installer.sh
+++ .ci/upload_win_installer.sh
@@ -6,6 +6,6 @@ tag=$(git describe --exact-match)
 [ "$tag" ] || exit
 curl -T 'packages\win_installer\output\Aegisub-Japan7-x64.exe' --user $FTP_USER:$FTP_PASS "$dest/Aegisub-Japan7-x64-${tag#v}.exe"
 
-printf "$tag\n$(git tag -l --format='%contents' $tag)" > latest
+printf "${tag#v}\n$(git tag -l --format='%contents' $tag)" > latest
 curl -T latest --user "$FTP_USER:$FTP_PASS" "$dest/"
 
