eab9e2cc096e657a59a8c546377aa68e5ac6f7b5
odrling <florianbadie@odrling.xyz>
2019-08-31 17:06:14 +0200

    fix upload script

--- .ci/upload_win_installer.sh
+++ .ci/upload_win_installer.sh
@@ -5,4 +5,5 @@ tag=$(git describe --exact-match)
 [ "$tag" ] || exit
 
 curl -T 'packages\win_installer\output\Aegisub-Japan7-x64.exe' --user $FTP_USER:$FTP_PASS "$dest/Aegisub-Japan7-x64-${tag#v}.exe"
-curl -T <(printf "${tag#v}\n$(git tag -l --format='%contents' $tag)") --user "$FTP_USER:$FTP_PASS" "$dest/"
+printf "${tag#v}\n$(git tag -l --format='%contents' $tag)" > latest
+curl -T latest --user "$FTP_USER:$FTP_PASS" "$dest/"
