7c69b2a9fc8731835d748dedc408f06db4be9f59
odrling <florianbadie@odrling.xyz>
2019-09-01 21:00:21 +0200

    put 'x64' after release version

--- .ci/upload_win_installer.sh
+++ .ci/upload_win_installer.sh
@@ -4,6 +4,6 @@ dest="ftp://shelter.mahoro-net.org/aegisub-japan7"
 tag=$(git describe --exact-match)
 [ "$tag" ] || exit
 
-curl -T 'packages\win_installer\output\Aegisub-Japan7-x64.exe' --user $FTP_USER:$FTP_PASS "$dest/Aegisub-Japan7-x64-${tag#v}.exe"
+curl -T 'packages\win_installer\output\Aegisub-Japan7-x64.exe' --user $FTP_USER:$FTP_PASS "$dest/Aegisub-Japan7-${tag#v}-x64.exe"
 printf "${tag#v}\n$(git tag -l --format='%contents' $tag)" > latest
 curl -T latest --user "$FTP_USER:$FTP_PASS" "$dest/"
