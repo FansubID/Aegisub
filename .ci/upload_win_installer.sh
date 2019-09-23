1817ba6631c642528fcdb80c8fb2dd1b08ba6b0a
odrling <florianbadie@odrling.xyz>
2019-09-23 14:21:11 +0200

    [ci] add redirection to latest version [skip ci]

--- .ci/upload_win_installer.sh
+++ .ci/upload_win_installer.sh
@@ -5,5 +5,10 @@ tag=$(git describe --exact-match)
 [ "$tag" ] || exit
 
 curl -T 'packages\win_installer\output\Aegisub-Japan7-x64.exe' --user $FTP_USER:$FTP_PASS "$dest/Aegisub-Japan7-${tag#v}-x64.exe"
+
 printf "${tag#v}\n$(git tag -l --format='%(contents)' $tag)" > latest
 curl -T latest --user "$FTP_USER:$FTP_PASS" "$dest/"
+
+url="Aegisub-Japan7-${tag#v}-x64.exe"
+printf "<!doctype html><html><head><meta http-equiv='refresh' content='0; url=$url' /></head><body><a href='$url'>$url</a></body></html>" > Aegisub-Japan7-latest-x64.exe
+curl -T Aegisub-Japan7-latest-x64.exe --user "$FTP_USER:$FTP_PASS" "$dest/"
