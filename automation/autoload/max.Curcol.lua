d4ca508927c87d5fcd325245fac0297040811d77
Iqbal Rifai <iqbl@nhent.ai>
2020-07-08 16:07:23 +0700

    add More Automation

---
+++ automation/autoload/max.Curcol.lua
@@ -0,0 +1,32 @@
+script_name="Curcol - {\\s1}Strikethrough{\\s0} Menjadi {Komentar} (seleksi)"
+script_description="Mengubah kata yang {\\s1}dicoret{\\s0} menjadi {komentar} dan sebaliknya."
+script_author="Max FireHeart"
+script_version="1.0"
+
+function curcol(subs, sel)
+    for x, i in ipairs(sel) do
+        line=subs[i]
+        text=line.text
+		:gsub("{\\s1}{([^\\}]-)}$","%1")
+
+		:gsub("{\\s[10]}{([^\\}]-)}","%1")
+
+		:gsub("}{\\s0}","")
+
+		:gsub("{\\s1}","{")
+
+		:gsub("{\\s0}","}")
+
+		:gsub("{([^\\{]-){([^\\}]-)}}","{%2}")
+		:gsub("{([^\\{}]-){([^\\}]-)}([^\\{}]-)}","{%1(x%2q)%3}")
+		:gsub("{([^\\{}]-){([^\\}]-)}([^\\{}]-){([^\\}]-)}([^\\{}]-)}","{%1(x%2q)%3(x%4q)%5}")		
+		:gsub("{([^\\}]-)}([^\\{}]-)}","(x%1q)%2")
+		:gsub("(%([^\\()]-%))(%([^\\()]-%))","%1")			
+	line.text=text
+        subs[i]=line
+    end
+    aegisub.set_undo_point(script_name)
+    return sel
+end
+
+aegisub.register_macro(script_name, script_description, curcol)
---
+++ automation/autoload/max.NoKomen.lua
@@ -0,0 +1,24 @@
+script_name="NoKomen - Hapus {Komentar} (seleksi)"
+script_description="Menghilangkan titik-titik-titik."
+script_author="Max FireHeart"
+script_version="1.0"
+
+function nokomen(subs, sel)
+    for x, i in ipairs(sel) do
+        line=subs[i]
+        text=line.text
+		:gsub("{[^\\}]-}","")
+
+		:gsub("{[^\\}]-\\N[^\\}]-}","")
+	    
+		:gsub("^({[^}]-}) *","%1")
+	    
+		:gsub(" *$","")			
+	line.text=text
+        subs[i]=line
+    end
+    aegisub.set_undo_point(script_name)
+    return sel
+end
+
+aegisub.register_macro(script_name, script_description, nokomen)
